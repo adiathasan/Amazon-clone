de3e06caa3b39e00505d2e71ecd22622218d49ea
adiathasan <adiathasan.me@gmail.com>
2020-09-08 19:37:00 -0700

    initial commit. project setup done

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,14 @@
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDRdY2_QDi2rx4845iATwLxSPyQmDGY0Qo",
+    authDomain: "clone-ad.firebaseapp.com",
+    databaseURL: "https://clone-ad.firebaseio.com",
+    projectId: "clone-ad",
+    storageBucket: "clone-ad.appspot.com",
+    messagingSenderId: "945735233335",
+    appId: "1:945735233335:web:aa35359a740c8915b1f4b2",
+    measurementId: "G-ZP7G0ZZGSZ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
